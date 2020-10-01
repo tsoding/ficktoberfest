d9657fcbeb0ad7c190f6694cf65ff8b79c31af7c
rexim <reximkut@gmail.com>
2020-10-02 00:21:09 +0700

    Watch only Pull Request events

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
@@ -19,10 +20,19 @@ import qualified Data.ByteString as B
 import Data.Char
 import Control.Concurrent
 import Control.Applicative
+import qualified Data.HashMap.Strict as HM
 
 newtype GithubToken = GithubToken T.Text
 newtype Owner = Owner T.Text
 newtype Repo = Repo T.Text
+
+data PullRequestEvent = PullRequestEvent
+  { pullRequestEventId :: T.Text
+  , pullRequestEventPull :: Pull
+  } deriving (Show)
+
+instance FromJSON PullRequestEvent where
+  parseJSON (Object v) = PullRequestEvent <$> v .: "id" <*> (v .: "payload" >>= (.: "pull_request"))
 
 data Event = Event
   { eventType :: T.Text
@@ -60,7 +70,7 @@ newtype ETag =
   ETag B.ByteString
   deriving (Show)
 
-pollEvents :: Github -> Owner -> Maybe ETag -> IO (Maybe ETag, Maybe Int, [Event])
+pollEvents :: Github -> Owner -> Maybe ETag -> IO (Maybe ETag, Maybe Int, [PullRequestEvent])
 pollEvents github@Github { githubToken = GithubToken token
                          , githubManager = manager
                          } (Owner owner) etag = do
@@ -83,20 +93,30 @@ pollEvents github@Github { githubToken = GithubToken token
     Status 304 _ -> return (etag', interval, [])
     Status 200 _ -> do
       events <- either error return $ eitherDecode $ responseBody response
-      return (etag', interval, events)
+      let pullRequestEvents =
+            mapMaybe
+              (\case
+                 v'@(Object v)
+                   | (HM.lookup "type" v) == Just (String "PullRequestEvent") ->
+                     case fromJSON v' of
+                       Success x -> Just x
+                       Error _ -> error "Failed to parse"
+                 _ -> Nothing)
+              events
+      return (etag', interval, pullRequestEvents)
     Status x _ -> error $ printf "Got unexpected status %d" x
 
 pollLoop :: Github -> Owner -> Maybe T.Text -> Maybe ETag -> IO ()
 pollLoop github owner lastEventId etag = do
   (etag', interval, events) <- pollEvents github owner etag
   let unprocessedEvents =
-        filter (\x -> lastEventId < Just (eventId x)) events
+        filter (\x -> lastEventId < Just (pullRequestEventId x)) events
   mapM print unprocessedEvents
   maybe (return ()) (threadDelay . (* 1000000)) interval
   pollLoop
     github
     owner
-    ((eventId <$> listToMaybe unprocessedEvents) <|> lastEventId)
+    ((pullRequestEventId <$> listToMaybe unprocessedEvents) <|> lastEventId)
     etag'
 
 getPullRequests :: Github -> Owner -> Repo -> Int -> IO [Pull]
@@ -129,6 +149,7 @@ getPullRequests github@Github { githubToken = GithubToken token
 mainWithArgs :: [String] -> IO ()
 mainWithArgs (tokenFile:_) = do
   github <- newGithub tokenFile
+  putStrLn "Waiting for Pull Requests..."
   pollLoop github (Owner "rexim") Nothing Nothing
 mainWithArgs _ = error "Usage: ficktoberfest <tokenFile>"
 
